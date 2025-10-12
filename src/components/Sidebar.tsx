dba9a9dab89bfbe51fdbb8494d8a9a26deb51330
greg <greg87peterson@gmail.com>
2025-10-11 21:21:49 -0700

    fixing sidebar img

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ className }) => {
   return (
     <aside className={`flex-shrink-0 h-[35vh] flex items-center md:h-screen w-full md:w-1/3 bg-gray-700 border-r border-cyan-400 flex items-center justify-center relative ${className || ""}`}>
-        <div className="flex flex-col items-center text-center >
+        <div className="flex flex-col items-center text-center">
             <Image
                 src="/profile.jpg"
                 alt="Profile"
